4f3d85ed13ccd06b93087453a8735019c36673b0
Vraj Patel <vcp37@scarletmail.rutgers.edu>
2024-08-28 00:37:27 -0400

    changes made firebase

--- firebase.js
+++ firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 
 import { getFirestore} from 'firebase/firestore' 
+console.log(hi)
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
